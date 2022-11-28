7f6e22e417b895c79b27aade4797ecef746a2fc5
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-11-28 13:10:46 +0100

    Craete operation function to perform selected operation

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -10,6 +10,31 @@ let inputX = parseInt(prompt("Insert your First Variable.: "));
 let inputY = parseInt(prompt("Insert your Second Variable.: "));
 
 // Create an operation function to perform selected operations on inputs.
+function operation(){
+    if(operator === "a"){
+        let sum = inputX + inputY;
+        console.log(`The Sum is: ${sum}`);
+        return sum;
+    } else if(operator === "b"){
+        let difference = inputX - inputY;
+        console.log(`The Difference is: ${difference}`);
+        return difference;
+    } else if(operator === "c"){
+        let product = inputX * inputY;
+        console.log(`The Product is: ${product}`);
+        return product;
+    } else if(operator === "d"){
+        let quotient = inputX / inputY;
+        console.log(`The Quotient is: ${quotient}`);
+        return quotient;
+    } else if(operator === "e"){
+        let modulo = inputX % inputY;
+        console.log(`The Modulus is: ${modulo}`);
+        return modulo;
+    } else{
+        console.log("Please Input a Valid Number");
+    }
+}
 // Create performOperation function to select and opeartor then perform the opeartion selected on inputs.
 // Create a function reset to clear all inputs and operators before starting another opeartion.
 // Create a function performAnotherOperation to prompt the user if he wants to carry out another opeartion and accept input.
