7d3951341577855b032f490e777c88c93d465a21
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-11-28 13:08:50 +0100

    Define prompt to accept input from console and decare operator, inputX and inputY variables

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -2,9 +2,14 @@
 // A calculator that performs additon, subtraction, multiplication, division and modulus operations on two inputs.
 
 // Define prompt to be able to insert inputs from console.
+const prompt = require("prompt-sync")();
+
 //Declare Variables for inputX, inputY and operator.
+let operator = (prompt("Welcome! Select an Operator (a).Addition, (b).Subtraction, (c).Multiplication, (d).Division, (e).Modulus.: ")).toLowerCase();
+let inputX = parseInt(prompt("Insert your First Variable.: "));
+let inputY = parseInt(prompt("Insert your Second Variable.: "));
+
 // Create an operation function to perform selected operations on inputs.
 // Create performOperation function to select and opeartor then perform the opeartion selected on inputs.
 // Create a function reset to clear all inputs and operators before starting another opeartion.
 // Create a function performAnotherOperation to prompt the user if he wants to carry out another opeartion and accept input.
- 
