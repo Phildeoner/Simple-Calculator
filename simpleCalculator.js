e673e3e36005fbc8ad0ab1fc8b1fc660aaf6eb68
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-11-28 13:06:25 +0100

    Update simpleCalculator.js with pseudocode

---
+++ simpleCalculator.js
@@ -0,0 +1,10 @@
+// Simple Calculator
+// A calculator that performs additon, subtraction, multiplication, division and modulus operations on two inputs.
+
+// Define prompt to be able to insert inputs from console.
+//Declare Variables for inputX, inputY and operator.
+// Create an operation function to perform selected operations on inputs.
+// Create performOperation function to select and opeartor then perform the opeartion selected on inputs.
+// Create a function reset to clear all inputs and operators before starting another opeartion.
+// Create a function performAnotherOperation to prompt the user if he wants to carry out another opeartion and accept input.
+ 
